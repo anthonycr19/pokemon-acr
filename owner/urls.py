35377914a810eb8e789d2a6b8fa5eac07cc8037c
“Anthony <acarrillorj@gmail.com>
2023-12-17 10:10:04 -0500

    uso de ORM's

--- owner/urls.py
+++ owner/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('owner_list/', views.owner_list, name='owner_list')
+    path('owner_list/', views.owner_list, name='owner_list'),
+    path('owner_orm/', views.owner_orm, name='owner_orm'),
+    #path('owner_detail/<int:id_owner>', views.owner_detail, name="owner_edit"),
 ]
+
